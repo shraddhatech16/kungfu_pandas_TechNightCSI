1a748f660241bdb115af305a284c41f7f770d67e
ritikajagtap <ritika.jagtap.2002@gmail.com>
2023-03-04 13:02:12 +0530

    cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar/Navbar';
+import Cards from './components/Cards/Cards';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+
     </div>
   );
 }
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Cards() {
+    return (
+        <>
+            <Stack direction="horizontal" gap={2}>
+                <Button as="a" variant="primary">
+                    Button as link
+                </Button>
+                <Button as="a" variant="success">
+                    Button as link
+                </Button>
+            </Stack>
+        </>
+    )
+}
+
+export default Cards
