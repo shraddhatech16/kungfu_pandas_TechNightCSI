5eed76d11df9ca35cf15f45738521958d9917dae
ritikajagtap <ritika.jagtap.2002@gmail.com>
2023-03-04 11:29:56 +0530

    navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './navbar.css';
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+      <div>
+        <a href="#">Logo</a>
+      </div>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/Navbar/Templates/Templates.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Template() {
+  return (
+    <div>
+        <h1>Choose a Template to create your own website</h1>
+    </div>
+  )
+}
+
+export default Template
