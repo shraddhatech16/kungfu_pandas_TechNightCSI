1155100e7d5f0f30490e7d4688df1defeda13b20
shraddhatech16 <rajendraalawane@gmail.com>
2023-03-04 12:59:14 +0530

    corousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import HomePage from './components/Homepage/Homepage';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <HomePage></HomePage>
+      
     </div>
   );
 }
